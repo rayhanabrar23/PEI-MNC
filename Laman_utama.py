28c518f5e86cd1ecc9876609bce816f7c5b7f2d0
rayhanabrar23 <rayhanabrar023@gmail.com>
2026-02-26 14:57:39 +0700

    Create Laman_utama.py

---
+++ Laman_utama.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="MNCN Project", layout="wide")
+
+st.title("Selamat Datang di Portal Data MNCN")
+st.write("Silakan pilih menu di sebelah kiri untuk mulai mengolah data.")
