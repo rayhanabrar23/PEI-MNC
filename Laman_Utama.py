6c3f4974c7f619c1af6318662e2773180fdcfa7f
rayhanabrar23 <rayhanabrar023@gmail.com>
2026-04-29 08:16:03 +0700

    Update Laman_Utama.py

--- Laman_Utama.py
+++ Laman_Utama.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="I-Fast Converter", layout="wide")
 
-st.title("Selamat Datang di PEI I-Fast Converter   -   MNC Sekuritas")
+st.title("Selamat Datang di PEI I-Fast Converter")
+st.subheader("MNC Sekuritas")
+
 st.write("Silakan pilih menu di sebelah kiri untuk mulai mengolah data.")
