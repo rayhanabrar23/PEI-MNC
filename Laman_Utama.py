207908f8ecb2e17e51f30b4f8963b2e5542040e5
rayhanabrar23 <rayhanabrar023@gmail.com>
2026-04-29 08:17:12 +0700

    Update Laman_Utama.py

--- Laman_Utama.py
+++ Laman_Utama.py
@@ -1,8 +1,36 @@
 import streamlit as st
 
-st.set_page_config(page_title="I-Fast Converter", layout="wide")
+# Menyisipkan font Roboto Condensed dari Google Fonts
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap');
 
-st.title("Selamat Datang di PEI I-Fast Converter")
-st.subheader("MNC Sekuritas")
+    /* Mengatur agar font diterapkan pada seluruh aplikasi atau elemen tertentu */
+    html, body, [class*="st-"] {
+        font-family: 'Roboto Condensed', sans-serif;
+    }
+    
+    .title-text {
+        font-family: 'Roboto Condensed', sans-serif;
+        font-weight: 700;
+        font-size: 42px;
+        margin-bottom: 0px;
+    }
+    
+    .subtitle-text {
+        font-family: 'Roboto Condensed', sans-serif;
+        font-weight: 400;
+        font-size: 24px;
+        color: #555;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Tampilan Judul
+st.markdown('<p class="title-text">Selamat Datang di PEI I-Fast Converter</p>', unsafe_allow_html=True)
+st.markdown('<p class="subtitle-text">MNC Sekuritas</p>', unsafe_allow_html=True)
 
 st.write("Silakan pilih menu di sebelah kiri untuk mulai mengolah data.")
